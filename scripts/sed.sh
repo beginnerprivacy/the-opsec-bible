e03262368512ffabbe5540bbc0bfeaa8ebc55534
anon <anon@nowhere.jez>
2025-06-22 15:15:49 +0200

    fix stuff

--- scripts/sed.sh
+++ scripts/sed.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-sed -i 's/The Nihilism Opsec Blog/The Opsec Bible/gi'	 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md
-sed -i 's/Nihilism Opsec Blog/Opsec Bible/gi'			 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md
-sed -i 's/The Opsec Blog/The Opsec Bible/gi'			 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md 
-sed -i 's/Opsec Blog/Opsec Bible/gi'					 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md	
+#sed -i 's/The Nihilism Opsec Blog/The Opsec Bible/gi'	 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md
+#sed -i 's/Nihilism Opsec Blog/Opsec Bible/gi'			 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md
+#sed -i 's/The Opsec Blog/The Opsec Bible/gi'			 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md 
+#sed -i 's/Opsec Blog/Opsec Bible/gi'					 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md	
 
 
 
-sed -i 's/The Opsec blog/The Opsec Bible/gi'			 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md 
-sed -i 's/Opsec blog/Opsec Bible/gi'					 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md	
+#sed -i 's/The Opsec blog/The Opsec Bible/gi'			 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md 
+#sed -i 's/Opsec blog/Opsec Bible/gi'					 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md	
+sed -i 's/nihilism blog/Opsec Bible/gi'					 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md	
+sed -i 's/Nihilism blog/Opsec Bible/gi'					 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md	
+sed -i 's/Nihilism Blog/Opsec Bible/gi'					 *.md */*.md */*/*.md */*/*/*.md */*/*/*/*.md	
